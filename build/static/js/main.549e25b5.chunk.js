c4ea4f9adf0471163ded0caa2009b9a19479d504
shamaayil ahmed <shamayilahmad12@gmail.com>
2020-12-29 16:32:00 +0530

    debugged countrypicker

--- build/static/js/main.549e25b5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcovid-19"]=this["webpackJsonpcovid-19"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(0),i=n(1),o=n.n(i),r=n(3),s=n.n(r);n(9);var d=function(){return Object(c.jsx)("div",{})};s.a.render(Object(c.jsx)(o.a.StrictMode,{children:Object(c.jsx)(d,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.549e25b5.chunk.js.map
